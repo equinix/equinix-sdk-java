9b1219558c5f647dca268e907181b9775fb2269e
tomasz.tutka <tomasz.tutka@eu.equinix.com>
2026-06-24 13:42:30 +0200

    added tests

--- services/fabricv4/src/main/java/com/equinix/sdk/fabricv4/model/InternetAccessRoutingProtocolStaticRequest.java
+++ services/fabricv4/src/main/java/com/equinix/sdk/fabricv4/model/InternetAccessRoutingProtocolStaticRequest.java
@@ -90,52 +90,6 @@ public class InternetAccessRoutingProtocolStaticRequest extends InternetAccessRo
     this.connections = connections;
   }
 
-  /**
-   * A container for additional, undeclared properties.
-   * This is a holder for any undeclared properties as specified with
-   * the 'additionalProperties' keyword in the OAS document.
-   */
-  private Map<String, Object> additionalProperties;
-
-  /**
-   * Set the additional (undeclared) property with the specified name and value.
-   * If the property does not already exist, create it otherwise replace it.
-   *
-   * @param key name of the property
-   * @param value value of the property
-   * @return the InternetAccessRoutingProtocolStaticRequest instance itself
-   */
-  public InternetAccessRoutingProtocolStaticRequest putAdditionalProperty(String key, Object value) {
-    if (this.additionalProperties == null) {
-        this.additionalProperties = new HashMap<String, Object>();
-    }
-    this.additionalProperties.put(key, value);
-    return this;
-  }
-
-  /**
-   * Return the additional (undeclared) property.
-   *
-   * @return a map of objects
-   */
-  public Map<String, Object> getAdditionalProperties() {
-    return additionalProperties;
-  }
-
-  /**
-   * Return the additional (undeclared) property with the specified name.
-   *
-   * @param key name of the property
-   * @return an object
-   */
-  public Object getAdditionalProperty(String key) {
-    if (this.additionalProperties == null) {
-        return null;
-    }
-    return this.additionalProperties.get(key);
-  }
-
-
   @Override
   public boolean equals(Object o) {
     if (this == o) {
@@ -146,13 +100,12 @@ public class InternetAccessRoutingProtocolStaticRequest extends InternetAccessRo
     }
     InternetAccessRoutingProtocolStaticRequest internetAccessRoutingProtocolStaticRequest = (InternetAccessRoutingProtocolStaticRequest) o;
     return Objects.equals(this.connections, internetAccessRoutingProtocolStaticRequest.connections)&&
-        Objects.equals(this.additionalProperties, internetAccessRoutingProtocolStaticRequest.additionalProperties) &&
         super.equals(o);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(connections, super.hashCode(), additionalProperties);
+    return Objects.hash(connections, super.hashCode());
   }
 
   @Override
@@ -161,7 +114,6 @@ public class InternetAccessRoutingProtocolStaticRequest extends InternetAccessRo
     sb.append("class InternetAccessRoutingProtocolStaticRequest {\n");
     sb.append("    ").append(toIndentedString(super.toString())).append("\n");
     sb.append("    connections: ").append(toIndentedString(connections)).append("\n");
-    sb.append("    additionalProperties: ").append(toIndentedString(additionalProperties)).append("\n");
     sb.append("}");
     return sb.toString();
   }
